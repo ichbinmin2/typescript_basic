85d7afd2634ec7dfea6bf0434a757d7aca58fb8c
JiyeonMin <ichbinmin2@gmail.com>
2022-07-24 20:30:35 +0900

    📝 section-2 : Object Type(복습)

--- section-02/basics-05-object-types/app.ts
+++ section-02/basics-05-object-types/app.ts
@@ -1,10 +1,10 @@
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-const person = {
+const person: {
+  name: string;
+  age: number; // 이는 타입스크립트의 표현법일 뿐, 새로운 객체 생성이 아님!
+} = {
+  // const person = {
   name: "Maximilian",
   age: 30,
 };
 
-console.log(person.nickname);
+console.log(person.name);
